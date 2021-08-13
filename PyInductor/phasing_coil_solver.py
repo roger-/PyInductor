7bae19569509b152ecb0ae0f66ade1c2e20ebf4e
Filip Valder <filip.valder@gmail.com>
2021-08-13 10:04:52 +0200

    Modify phasing coil solver + add code comments

--- PyInductor/phasing_coil_solver.py
+++ PyInductor/phasing_coil_solver.py
@@ -11,10 +11,12 @@ warnings.filterwarnings("ignore")
 
 
 def _solver(combination):
+    # extract the changing params
     n, diam_mm, len_um, coil_parms = combination
+    # extract the static params
     params = {'N': n,
               'diam_former': (diam_mm + coil_parms['diam_wire_with_isol_mm']) * 1e-3,
-              'diam_wire': coil_parms['diam_wire_core'],
+              'diam_wire': coil_parms['diam_wire_core_mm'] * 1e-3,
               'f': coil_parms['frequency'],
               'len_coil': len_um * 1e-6}
     params.update(MATERIALS['Cu, annealed'])
@@ -24,26 +26,47 @@ def _solver(combination):
     except RuntimeError:
         return None
 
-    x = results['prop_factor'] * len_um * 1e-6
-    if (coil_parms['phase_shift_rad'] * (1 - coil_parms['phase_shift_tolerance_pct'] / 100)) < x < (
+    phi = results['prop_factor'] * len_um * 1e-6
+    # if phi is within the allowed tolerance
+    if (coil_parms['phase_shift_rad'] * (
+            1 - coil_parms['phase_shift_tolerance_pct'] / 100)) < phi < (
             coil_parms['phase_shift_rad'] * (1 + coil_parms['phase_shift_tolerance_pct'] / 100)):
 
         # we only want 1 wire layer for the winding, don't we?
-        len_mm = len_um * 1e-3
-        if len_mm < coil_parms['diam_wire_with_isol_mm'] * n:
+        if ind.turn_spacing <= 0:
             return None
 
-        return "N={n}, diam_mm={diam_mm}, len_mm={len_mm}, phi={x}".format(
-            n=n, diam_mm=diam_mm, len_mm=len_mm, x=x)
+        # return coil parameters: N of threads, diameter, length and the resulting phi
+        return "N={n}, diam_mm={diam_mm}, len_mm={len_mm}, phi={phi}".format(
+            n=n, diam_mm=diam_mm, len_mm=len_um * 1e-3, phi=phi)
 
 
-class Coil:
-    def __init__(self, phase_shift_rad, phase_shift_tolerance_pct, frequency, diam_wire_core,
+class PhasingCoilSolver:
+    def __init__(self, phase_shift_rad, phase_shift_tolerance_pct, frequency, diam_wire_core_mm,
                  diam_wire_with_isol_mm, N_range, diams_mm, len_range_mm, ncpus=0):
+        """
+        Parameters:
+        phase_shift_rad (float): Phase shift we want to achieve
+        phase_shift_tolerance_pct (float): Relative allowed phase shift difference (+/- percent)
+        frequency (float): center frequency
+        diam_wire_core_mm (float): Diameter of the wire's core
+        diam_wire_with_isol_mm (float): Diameter of the wire including insulation
+        N_range (tuple): Range of coil threads for which to perform the calculations,
+                         e.g. (10, 100)
+        diams_mm (list): List of coil diameters, e.g. [16, 20, 25, 32, 100, 125, 1500]
+                         This makes better sance than defining a range, because available
+                         tubes/pipes have fixed diameter and we usually want to choose one of
+                         them, rather than manufacture a pipe/tube with a specific diameter.
+        len_range_mm (tuple): Range of coil lenghts for which to perform the calculations,
+                              incl. step, e.g. (20, 300, 1)
+        ncpus (int): Number of CPUs we want to utilize; if set to zero, it defaults to
+                     number of available CPUs minus one which is reasonable for most cases.
+        """
+
         self.phase_shift_rad = phase_shift_rad
         self.phase_shift_tolerance_pct = phase_shift_tolerance_pct
         self.frequency = frequency
-        self.diam_wire_core = diam_wire_core
+        self.diam_wire_core_mm = diam_wire_core_mm
         self.diam_wire_with_isol_mm = diam_wire_with_isol_mm
         self.N_range = N_range
         self.diams_mm = diams_mm
@@ -55,18 +78,20 @@ class Coil:
 
     def solve(self):
 
-        ncpus = (cpu_count() - 1) if cpu_count() >= 2 else 1
-        pool = Pool(processes=ncpus)
+        pool = Pool(processes=self.ncpus)
 
         len_range_um = (int(l_mm * 1e3) for l_mm in self.len_range_mm)
+        # generate all possible combinations
         combinations = product(range(*self.N_range), self.diams_mm, range(*len_range_um))
-        combinations = [c + (self.__dict__,) for c in combinations]
+        # attach coil static parameters to the combinations
+        combinations_with_static_params = [c + (self.__dict__,) for c in combinations]
 
         print("{begin_end} Processing started with {ncpus} procs {begin_end}".format(
-            begin_end=10 * "-", ncpus=ncpus))
+            begin_end=10 * "-", ncpus=self.ncpus))
         dt_start = datetime.now()
 
-        for out in pool.imap_unordered(_solver, combinations):
+        # feed the process pool with the data (generated combinations + static coil params)
+        for out in pool.imap_unordered(_solver, combinations_with_static_params):
             if out:
                 print(out)
 
--- tests/test_phasing_coil_solver.py
+++ tests/test_phasing_coil_solver.py
@@ -2,16 +2,16 @@ import sys
 import pytest
 
 from math import pi
-from PyInductor.phasing_coil_solver import Coil
+from PyInductor.phasing_coil_solver import PhasingCoilSolver
 
 
 @pytest.mark.skipif(sys.version_info >= (3, 0), reason="requires python2.7")
 def test_phasing_coil_solver_py2(capsys):
-    c = Coil(
+    c = PhasingCoilSolver(
         phase_shift_rad=pi,
         phase_shift_tolerance_pct=0.5,
         frequency=27e6,
-        diam_wire_core=0.4e-3,
+        diam_wire_core_mm=0.4,
         diam_wire_with_isol_mm=2.7,
         N_range=(95, 100),
         diams_mm=[32],
@@ -26,11 +26,11 @@ def test_phasing_coil_solver_py2(capsys):
 
 @pytest.mark.skipif(sys.version_info < (3, 8), reason="requires python3.8 or higher")
 def test_phasing_coil_solver_py3(capsys):
-    c = Coil(
+    c = PhasingCoilSolver(
         phase_shift_rad=pi,
         phase_shift_tolerance_pct=0.5,
         frequency=27e6,
-        diam_wire_core=0.4e-3,
+        diam_wire_core_mm=0.4,
         diam_wire_with_isol_mm=2.7,
         N_range=(95, 100),
         diams_mm=[32],
